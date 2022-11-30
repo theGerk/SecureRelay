182f4c13aff0b8ed4a5af49c3d4cb5cf093e19ed
Benji Altman <benji@altmansoftwaredesign.com>
2022-11-29 23:57:59 -0600

    single go client

--- SecureRelay/SecureRelayClient.cs
+++ SecureRelay/SecureRelayClient.cs
@@ -12,12 +12,19 @@ using Gerk.Crypto.EncryptedTransfer;
 
 namespace SecureRelay
 {
-	public class SecureRelayClient : IAsyncDisposable
+	public class SecureRelayClient
 	{
-		private volatile bool active = true;
-		private uint count = 0;
-		private TaskCompletionSource activeTaskCompletionSource = new TaskCompletionSource();
-		private readonly object lockobj = new();
+		/// <summary>
+		/// State of the connection. Starts at waiting, then starts relaying until the connection is closed.
+		/// </summary>
+		public enum State { WaitingForConnection, Relaying, Closed };
+		public State CurrentState { get; private set; } = State.WaitingForConnection;
+
+		private readonly TaskCompletionSource WhenConnectedTaskSource = new();
+		private readonly TaskCompletionSource WhenCompletedTaskSource = new();
+
+		public Task WhenConnected => WhenConnectedTaskSource.Task;
+		public Task WhenCompleted => WhenCompletedTaskSource.Task;
 
 		private SecureRelayClient() { }
 
@@ -28,46 +35,23 @@ namespace SecureRelay
 			return output;
 		}
 
-		public async ValueTask DisposeAsync()
-		{
-			lock (lockobj)
-			{
-				active = false;
-			}
-			await activeTaskCompletionSource.Task;
-		}
-
 		private async void Run(IPEndPoint listeningIp, IPEndPoint secureRemoteServer, IPEndPoint finalIpTarget, RSACryptoServiceProvider myKey, IEnumerable<IHasPublicKeySha> validTargets)
 		{
 			var tcpListener = new TcpListener(listeningIp);
+			tcpListener.Start();
 
-			while (active)
-			{
-				var localInsecureConnection = await tcpListener.AcceptTcpClientAsync();
-				lock (lockobj)
-				{
-					if (!active)
-						return;
-					++count;
-				}
+			var localInsecureConnection = await tcpListener.AcceptTcpClientAsync();
+			tcpListener.Stop();
+			CurrentState = State.Relaying;
+			WhenConnectedTaskSource.SetResult();
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-				SecureRelay.FromInsecureToSecure(localInsecureConnection.GetStream(), secureRemoteServer, finalIpTarget, myKey, validTargets)
-					.Then(() =>
-					{
-						uint count;
-						bool active;
-						lock (lockobj)
-						{
-							count = --this.count;
-							active = this.active;
-						}
-
-						if (count == 0 && !active)
-							activeTaskCompletionSource.SetResult();
-					});
+			SecureRelay.FromInsecureToSecure(localInsecureConnection.GetStream(), secureRemoteServer, finalIpTarget, myKey, validTargets)
+				.Then(() => {
+					CurrentState = State.Closed;
+					WhenCompletedTaskSource.SetResult();
+				});
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			}
 		}
 	}
 }
